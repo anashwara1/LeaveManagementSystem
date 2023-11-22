1cfdfc696c0f9bdc7e496d53710f1817ad3c784d
anashwara1 <anu@smalldaytech.com>
2023-11-22 20:37:05 +0530

    resolved the conflicts

--- LMS/myapp/urls.py
+++ LMS/myapp/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('applyleave/', views.applyleave, name='applyleave'),
     path('leavehistory/', views.leavehistory, name='leavehistory'),
     path('profile/', views.profile, name='profile'),
-    path('employee/', views.emppage, name='employee'),
+    path('employees/', views.emppage, name='employee'),
 ]
